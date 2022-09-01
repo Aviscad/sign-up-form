16fddf7a0e8540caabdc1d53da0b03098d99f900
Aviscad <anthonyviscad@gmail.com>
2022-09-01 16:44:06 -0600

    Creating custom validation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  //Variables
+  let name = document.getElementById("name"),
+    lastName = document.getElementById("last-name"),
+    email = document.getElementById("email"),
+    phone = document.getElementById("phone"),
+    password = document.getElementById("password"),
+    confirmPassword = document.getElementById("c-password");
+});
