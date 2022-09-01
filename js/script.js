b47281db5ba4152af54cf9c743d658f314429868
Aviscad <anthonyviscad@gmail.com>
2022-09-01 17:37:39 -0600

    Matching password validation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-window.addEventListener("load", () => {
-  //Variables
-  let name = document.getElementById("name"),
-    lastName = document.getElementById("last-name"),
-    email = document.getElementById("email"),
-    phone = document.getElementById("phone"),
-    password = document.getElementById("password"),
-    confirmPassword = document.getElementById("c-password");
+let password = document.getElementById("password"),
+  confirmPassword = document.getElementById("c-password"),
+  error = document.getElementById("errors"),
+  formSubmit = document.getElementById("form");
+
+formSubmit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (password.value.length < 4 || confirmPassword.value.length < 4) {
+    error.innerText = "Password must be at least 4 characters";
+  } else {
+    if (password.value !== confirmPassword.value) {
+      error.innerText = "Password does not match!";
+      error.style.color = "red";
+    } else {
+      error.innerText = "Information was saved!";
+      error.style.color = "green";
+    }
+  }
 });
